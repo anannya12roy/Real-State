c79f058f38f455466f7d35a22e7972e71507f3a4
Anannya Roy <roy12anna@gmail.com>
2024-11-21 17:12:21 +0600

    contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,37 @@ import TeamSection2 from "../svg/TeamSection2";
 import TeamSection3 from "../svg/TeamSection3";
 import TeamSection4 from "../svg/TeamSection4";
 
+function Title({data}){
+  return(
+    <h4 className="mb-9 text-lg font-semibold text-white">
+                {data.title}
+              </h4>
+  );
+}
+
+function AboutUs({data}){
+  return(
+    <li>
+      <a href="javascript:void(0)"className="mb-3 inline-block text-base text-gray-7 hover:text-primary">
+                    {data.title}
+                  </a>
+                </li>
+  );
+}
+
+function Features({data}){
+  return(
+    <li>
+                  <a
+                    href="javascript:void(0)"
+                    className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
+                  >
+                    {data.title}
+                  </a>
+                </li>
+  );
+}
+
 export default function Footer() {
   return (
     <footer className=" relative z-10 bg-[#090E34] pt-20 lg:pt-[100px]">
@@ -54,84 +85,50 @@ export default function Footer() {
           </div>
           <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
             <div className="mb-10 w-full">
-              <h4 className="mb-9 text-lg font-semibold text-white">
-                About Us
-              </h4>
+              <Title data={{
+                title: "About US"
+              }} />
               <ul>
-                <li>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
-                  >
-                    Features
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
-                  >
-                    About
-                  </a>
-                </li>
+                <AboutUs data={{
+                  title: "Home"
+                }} />
+                <AboutUs data={{
+                  title: "Features"
+                }} />
+                <AboutUs data={{
+                  title: "About"
+                }} />
               </ul>
             </div>
           </div>
           <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-3/12 xl:w-2/12">
             <div className="mb-10 w-full">
-              <h4 className="mb-9 text-lg font-semibold text-white">
-                Features
-              </h4>
+              <Title data={{
+                title: "Features"
+              }} />
               <ul>
-                <li>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
-                  >
-                    How it works
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
-                  >
-                    Privacy policy
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
-                  >
-                    Terms of Service
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="javascript:void(0)"
-                    className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
-                  >
-                    Refund policy
-                  </a>
-                </li>
+                <Features data={{
+                  title: "How it works"
+                }} />
+                <Features data={{
+                  title: "Privacy policy"
+                }} />
+                 <Features data={{
+                  title: "Terms of Service"
+                }} />
+                <Features data={{
+                  title: "Refund policy"
+                }} />
               </ul>
             </div>
           </div>
 
           <div className="w-full px-4 md:w-2/3 lg:w-6/12 xl:w-3/12">
             <div className="mb-10 w-full">
-              <h4 className="mb-9 text-lg font-semibold text-white">
-                Latest blog
-              </h4>
+              <Title data={{
+                title: "Latest blog"
+              }} />
+              
               <div className="flex flex-col gap-8">
                 <a
                   href="blog-details.html"
