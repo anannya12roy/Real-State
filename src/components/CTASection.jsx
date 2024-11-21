bb6cf764e3596a70ab57005d77491ca881b7aac7
Anannya Roy <roy12anna@gmail.com>
2024-11-21 14:17:10 +0600

    CTA section

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -1,3 +1,6 @@
+import CTASesction1 from "../svg/CTASection1";
+import CTASesction2 from "../svg/CTASection2";
+
 export default function CTASection() {
   return (
     <section className="relative z-10 overflow-hidden bg-primary py-20 lg:py-[115px]">
@@ -29,67 +32,10 @@ export default function CTASection() {
       </div>
       <div>
         <span className="absolute left-0 top-0">
-          <svg
-            width="495"
-            height="470"
-            viewBox="0 0 495 470"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="55"
-              cy="442"
-              r="138"
-              stroke="white"
-              strokeOpacity="0.04"
-              strokeWidth="50"
-            />
-            <circle
-              cx="446"
-              r="39"
-              stroke="white"
-              strokeOpacity="0.04"
-              strokeWidth="20"
-            />
-            <path
-              d="M245.406 137.609L233.985 94.9852L276.609 106.406L245.406 137.609Z"
-              stroke="white"
-              strokeOpacity="0.08"
-              strokeWidth="12"
-            />
-          </svg>
+          <CTASesction1 />
         </span>
         <span className="absolute bottom-0 right-0">
-          <svg
-            width="493"
-            height="470"
-            viewBox="0 0 493 470"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="462"
-              cy="5"
-              r="138"
-              stroke="white"
-              strokeOpacity="0.04"
-              strokeWidth="50"
-            />
-            <circle
-              cx="49"
-              cy="470"
-              r="39"
-              stroke="white"
-              strokeOpacity="0.04"
-              strokeWidth="20"
-            />
-            <path
-              d="M222.393 226.701L272.808 213.192L259.299 263.607L222.393 226.701Z"
-              stroke="white"
-              strokeOpacity="0.06"
-              strokeWidth="13"
-            />
-          </svg>
+         <CTASesction2 />
         </span>
       </div>
     </section>
